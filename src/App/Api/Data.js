05c4a934ad60cb0542cbc511a9dbf41de8f51aa2
Mukesh <mukeshkamlesh.kumar@gmail.com>
2019-08-17 01:04:08 +0530

    style bug fixed

--- src/App/Api/Data.js
+++ src/App/Api/Data.js
@@ -55,7 +55,20 @@ export const jsonResponse = [
     "company": "Source Club",
     "address": "4379 Simons Hollow Road Auburn, PA 17922"
   },
-  
+  {
+    "name": "John M. Upton",
+    "email": "JohnMUpton@jourrapide.com",
+    "phone":"818-599-4333",
+    "company": "Beaver Lumber",
+    "address": "4929 Joy Lane City Of Commerce, CA 90040"
+  },
+  {
+    "name": "Brian E. Miller",
+    "email": "BrianEMiller@armyspy.com",
+    "phone":"570-754-7206",
+    "company": "Source Club",
+    "address": "4379 Simons Hollow Road Auburn, PA 17922"
+  },
 
 ]
   
